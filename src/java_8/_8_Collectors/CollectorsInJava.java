a2125c574f67938e6ea15c89aec977a8291a098b
aulanand <atulanandmishra@gamil.com>
2024-06-17 09:00:53 +0530

    Stream API

---
+++ src/java_8/_8_Collectors/CollectorsInJava.java
@@ -0,0 +1,7 @@
+package java_8._8_Collectors;
+
+/**
+ * @author atquil
+ */
+public class CollectorsInJava {
+}
---
+++ src/java_8/_8_Stream_API/StreamAPI.java
@@ -0,0 +1,7 @@
+package java_8._8_Stream_API;
+
+/**
+ * @author atquil
+ */
+public class StreamAPI {
+}
